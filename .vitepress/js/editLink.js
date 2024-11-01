7e3505c51c4f56cf1c2156a6c87068ce31965c8b
Glendia <renqueshe@gmail.com>
2024-11-02 01:44:02 +0800

    重构内容

---
+++ .vitepress/js/editLink.js
@@ -0,0 +1,4 @@
+export const editLink = {
+  pattern: "https://github.com/Glendia/VitepressTemplate/edit/master/:path",
+  text: "在GitHub上编辑此页面",
+};
---
+++ .vitepress/js/footer.js
@@ -0,0 +1,5 @@
+export const footer = {
+  message: "基于 MIT 许可发布",
+  copyright:
+    'Copyright © 2022-2024 <a href="/">Glendia</a> All Rights Reserved.',
+};
